18124c9305c1721dc997bd264940cab28df11bcb
Andrew van Breda <andrew_van_breda@hotmail.com>
2016-09-06 15:17:27 +0100

    Plant Portal - Changes all groups to be created automatically and assigned to the user during import.

--- prebuilt_forms/js/plant_portal_user_data_importer.js
+++ prebuilt_forms/js/plant_portal_user_data_importer.js
@@ -1,27 +1,18 @@
+//Send new plots to the warehouse, currently this is just a placeholder function.
 function send_new_plots_to_warehouse(warehouseUrl,websiteId,plotNamesToProcess,plotSrefsToProcess,plotSrefSystemsToProcess) {
   alert('Sending the following new plots to the warehouse '+plotNamesToProcess.toSource());
 }
 
-function send_new_groups_to_warehouse(warehouseUrl,websiteId,groupNamesToProcess,groupTermlistId) {
-  alert('Sending the follow new groups to the warehouse termlist id '+groupTermlistId+' groups are, '+groupNamesToProcess.toSource());
-  /*var i;
-  var rawNamesArray=[];
-  for (i=0; i<groupsToProcess.length;i++) {
-    rawNamesArray[i]=groupsToProcess[i];
-  }
-  uploadNewGroup(warehouseUrl,rawNamesArray,groupTermlistId);*/
-}
-
-uploadNewGroup = function(warehouseUrl,rawNamesArray,groupTermlistId) {
-  /*var i;
-  for (i=0; i<rawNamesArray.length;i++) {
-    alert(rawNamesArray[i]);
+//Send any new groups to the warehouse. Used for both plot and sample groups.
+function send_new_groups_to_warehouse(warehouseUrl,websiteId,groupNamesToProcess,groupType,userId,personAttributeIdThatHoldsGroupsForUser) {
+  alert('Sending the follow new '+groupType+' to the warehouse, the groups are, '+groupNamesToProcess.toSource());
+  var i;
+  for (i=0; i<groupNamesToProcess.length;i++) {
     jQuery.ajax({
-      url: warehouseUrl+'index.php/services/plant_portal_import/create_new_group?name='+rawNamesArray[i]+'&termlist_id='+groupTermlistId,
+      url: warehouseUrl+'index.php/services/plant_portal_import/create_new_group?name='+groupNamesToProcess[i]+'&groupType='+groupType+'&userId='+userId+'&personAttributeId='+personAttributeIdThatHoldsGroupsForUser,
       dataType: 'jsonp',
       success: function(response) {
-        //$('#progress-bar').progressbar ('option', 'value', response.progress);
       }
     });
-  }*/
+  }
 }
