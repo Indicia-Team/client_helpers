312d525e9f1137c38bb9c1cc3fb491adcbbf6d43
Andrew van Breda <andrew_van_breda@hotmail.com>
2016-09-07 09:49:02 +0100

    Plant Portal - Send group names in chunks to warehouse for efficiency

--- prebuilt_forms/js/plant_portal_user_data_importer.js
+++ prebuilt_forms/js/plant_portal_user_data_importer.js
@@ -7,9 +7,34 @@ function send_new_plots_to_warehouse(warehouseUrl,websiteId,plotNamesToProcess,p
 function send_new_groups_to_warehouse(warehouseUrl,websiteId,groupNamesToProcess,groupType,userId,personAttributeIdThatHoldsGroupsForUser) {
   alert('Sending the follow new '+groupType+' to the warehouse, the groups are, '+groupNamesToProcess.toSource());
   var i;
+  //To improve efficiency we send the new groups in chunks to the warehouse. e.g. this might be 5 at a time comma separated
+  var groupNamesToProcessChunks=[];
+  var groupNamesToProcessChunksIdx = 0;
+  //Track number of groups in a chunk
+  var chunkSizeCounter=1;
+  //Cycle through each group name.
+  //We add it to the chunk, if we detect the number of items in the chunk is now too great we start a new chunk and reset the counter that checks its size
   for (i=0; i<groupNamesToProcess.length;i++) {
+    if (groupNamesToProcessChunks[groupNamesToProcessChunksIdx]) {
+      groupNamesToProcessChunks[groupNamesToProcessChunksIdx]=groupNamesToProcessChunks[groupNamesToProcessChunksIdx]+','+groupNamesToProcess[i];
+    } else {
+       groupNamesToProcessChunks[groupNamesToProcessChunksIdx]=groupNamesToProcess[i];
+    }
+
+    //If chunk gets too big start a new chunk and rest the chunk size counter to 0
+    //Currently the chunks are of 5 names, this can be altered by changing this number.
+    if (chunkSizeCounter>=5) {
+      chunkSizeCounter=1;
+      groupNamesToProcessChunksIdx++;
+    } else {
+      //Each time we add a group to a chunk, then increase the counter which tracks the size of the chunk
+      chunkSizeCounter++;
+    }
+  }
+  //Send each chunk to the warehouse
+  for (i=0; i<groupNamesToProcessChunks.length;i++) {
     jQuery.ajax({
-      url: warehouseUrl+'index.php/services/plant_portal_import/create_new_group?name='+groupNamesToProcess[i]+'&groupType='+groupType+'&userId='+userId+'&personAttributeId='+personAttributeIdThatHoldsGroupsForUser,
+      url: warehouseUrl+'index.php/services/plant_portal_import/create_new_group?names='+groupNamesToProcessChunks[i]+'&groupType='+groupType+'&userId='+userId+'&personAttributeId='+personAttributeIdThatHoldsGroupsForUser,
       dataType: 'jsonp',
       success: function(response) {
       }
