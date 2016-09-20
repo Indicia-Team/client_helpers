2a26710b706053831b729da2951fbb6c15084875
John van Breda <john.vanbreda@biodiverseit.co.uk>
2016-09-20 11:30:11 +0100

    ES Lint

--- prebuilt_forms/extensions/pantheon.js
+++ prebuilt_forms/extensions/pantheon.js
@@ -1,21 +1,25 @@
 // retrieve a query string parameter
 function getParameterByName(name) {
+  var regex;
+  var results;
+  var searchname = name;
   // special case - dynamic-sample_id can be provided as params table=sample&id=...
-  if (name==='dynamic-sample_id' && location.search.match(/table=sample/)) {
-    name='id';
+  if (name === 'dynamic-sample_id' && location.search.match(/table=sample/)) {
+    searchname = 'id';
   }
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  searchname = searchname.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  regex = new RegExp('[\\?&]' + searchname + '=([^&#]*)');
+  results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
-jQuery(document).ready(function($) {
-  var dirty=window.location.href.match(/\?q=/),
-    q=dirty ? '?q=' : '', join;
+jQuery(document).ready(function ($) {
+  var dirty = window.location.href.match(/\?q=/);
+  var q = dirty ? '?q=' : '';
+  var join;
   // Fix up all pantheon links
-  $.each($('.button-links a'), function() {
-    join = ($(this).attr('href').match(/\?/) || q!=='') ? '&' : '?';
+  $.each($('.button-links a'), function () {
+    join = ($(this).attr('href').match(/\?/) || q !== '') ? '&' : '?';
     $(this).attr('href', q + $(this).attr('href') + join + 'dynamic-sample_id=' + getParameterByName('dynamic-sample_id'));
   });
 });
