a5df79a26d7a82ab20b1dbaba7885f5652834243
John van Breda <john.vanbreda@biodiverseit.co.uk>
2016-11-19 16:19:07 +0000

    Ensures tickboxes stay active
    Fixes https://github.com/BiologicalRecordsCentre/iRecord/issues/93

--- prebuilt_forms/js/verification_5.js
+++ prebuilt_forms/js/verification_5.js
@@ -41,6 +41,9 @@ var rowIdToReselect = false;
       }
       rowIdToReselect = false;
     }
+    if (multimode) {
+      showTickList();
+    }
   };
 
   mapInitialisationHooks.push(function (div) {
