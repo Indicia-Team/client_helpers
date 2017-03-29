cac7c5a906ed0e07be9680d6b51323a828ad9714
Gary van Breda <gary.h@vbitservices.com>
2017-03-29 14:03:21 +0100

    UKBMS Section Plot form
    Allow interaction with report group cookie to share location_type_id
    parameter with other forms.
    Improve UX

--- prebuilt_forms/ukbms_section_plot.php
+++ prebuilt_forms/ukbms_section_plot.php
@@ -134,6 +134,27 @@ class iform_ukbms_section_plot {
       		'type'=>'report_helper::report_picker',
       		'group'=>'Report Settings'
       	),
+      		array(
+      				'name' => 'report_group',
+      				'caption' => 'Report group',
+      				'description' => 'When using several reports on a single page (e.g. <a href="http://code.google.com/p/indicia/wiki/DrupalDashboardReporting">dashboard reporting</a>) '.
+      				'you must ensure that all reports that share a set of input parameters have the same report group as the parameters report.',
+      				'type' => 'text_input',
+      				'default' => 'report',
+      				'group' => 'Report Settings'
+      		), array(
+      				'name' => 'remember_params_report_group',
+      				'caption' => 'Remember report parameters group',
+      				'description' => 'Enter any value in this parameter to allow the report to save its parameters for the next time the report is loaded. '.
+      				'The parameters are saved site wide, so if several reports share the same value and the same report group then the parameter '.
+      				'settings will be shared across the reports even if they are on different pages of the site. This functionality '.
+      				'requires cookies to be enabled on the browser.',
+      				'type'=>'text_input',
+      				'required'=>false,
+      				'default' => '',
+      				'group'=>'Report Settings'
+      		),
+
       	array(
       		'name'=>'countOccAttrId',
       		'caption'=>'Count Occurrence Attribute',
@@ -481,9 +502,20 @@ class iform_ukbms_section_plot {
     // loop through all entries in the locationTypesFilter, and build an array of locations.
     $locationTypeLookUpValues = array();
     $default = false;
+    if (isset($_COOKIE['providedParams']) && !empty($args['remember_params_report_group'])) {
+      $cookieData = json_decode($_COOKIE['providedParams'], true);
+      // guard against a corrupt cookie
+      if (is_array($cookieData) && !empty($cookieData[$args['remember_params_report_group']])) {
+        $cookieParams = $cookieData[$args['remember_params_report_group']];
+        $locationTypeKey = (isset($args['report_group']) ? $args['report_group'].'-' : '').'location_type_id';
+        if (is_array($cookieParams) && isset($cookieParams[$locationTypeKey])) {
+          $default = $cookieParams[$locationTypeKey];
+        }
+      }
+    }
     foreach(array_keys($options['surveyMapping']) as $location_type_id) {
       if(!$default)
-      	$default = $location_type_id;
+        $default = $location_type_id;
       $attrArgs['location_type_id'] = $location_type_id;
       $attrArgs['survey_id'] = $options['surveyMapping'][$location_type_id]['survey_id'];
       $locationListArgs['extraParams']['location_type_id'] = $location_type_id;
@@ -543,7 +575,7 @@ class iform_ukbms_section_plot {
       natcasesort($sort);
       $ctrl .='<select id="'.$options['locationSelectIDPrefix'].'-'.$location_type_id.'" class="location-select">';
       if(count($locs)>0) {
-      	$ctrl .= '<option value="" class="location-select-option" >&lt;'.lang::get('Please select').' : '.$options['surveyMapping'][$location_type_id]['location_type_term'].'&gt;</option>';
+      	$ctrl .= '<option value="" class="location-select-option italic-option" >&lt;'.lang::get('Please select').' : '.$options['surveyMapping'][$location_type_id]['location_type_term'].'&gt;</option>';
       	foreach($sort as $id=>$name){
       		$ctrl .= '<option value='.$id.' class="location-select-option '.
       			(!empty($args['sensitivityLocAttrId']) && $locs[$id]['attr_location_'.$args['sensitivityLocAttrId']] === "1" ? 'sensitive' : '').
@@ -552,7 +584,7 @@ class iform_ukbms_section_plot {
       			'</option>';
       	}
       } else 
-      	$ctrl .= '<option value="" class="location-select-option" >&lt;'.lang::get('No locations available').'&gt;</option>';
+      	$ctrl .= '<option value="" class="location-select-option" >&lt;'.lang::get('No sites available').'&gt;</option>';
       $ctrl .='</select>';
     }
     // default location type is the first in the list, so populate its locations as default as well.
@@ -562,13 +594,14 @@ class iform_ukbms_section_plot {
     			'id' => $options['locationTypeSelectID'],
     			'fieldname' => 'location_type_id',
     			'lookupValues' => $locationTypeLookUpValues,
-    			'default' => $default
+    			'default' => $default,
+    			'validation' => array('required')
     	)).'</th><th>'.$ctrl;
     } else {
     	$ctrl .= '<input type="hidden" id="'.$options['locationTypeSelectID'].'" name="location_type_id" value="'.$default.'" />';
     }
 
-    return $ctrl;
+    return $ctrl.'<span class="deh-required">*</span>';
   }
 
   private static function _species1_control($args, $readAuth, $nid, $options)
@@ -607,7 +640,7 @@ class iform_ukbms_section_plot {
 	for($i = $now->format('Y'); $i >= $args['first_year']; $i--){
 		$r .= '<option value="'.$i.'">'.$i.'</option>';
     }
-    $r .= '</select>';
+    $r .= '</select><span class="deh-required">*</span>';
     return $r;
   }
 
@@ -616,7 +649,7 @@ class iform_ukbms_section_plot {
   	$partsA=explode(':',$args['inSeasonWeeks']);
   	$partsB=explode(':',$args['allWeeks']);
   	$r = '<label for="'.$options['weekNumSelectID'].'">'.lang::get("Week Number").':</label><select id="'.$options['weekNumSelectID'].'" name="week">' .
-			'<option value="season" class="week-select-option" >'.lang::get('All in-season weeks').' ('.$partsA[0].' '.lang::get('to').' '.$partsA[1].')</option>' .
+			(($partsA[0] != $partsB[0] || $partsA[1] != $partsB[1]) ? '<option value="season" class="week-select-option" >'.lang::get('All in-season weeks').' ('.$partsA[0].' '.lang::get('to').' '.$partsA[1].')</option>' : '').
 			'<option value="all" class="week-select-option" >'.lang::get('All weeks').' ('.$partsB[0].' '.lang::get('to').' '.$partsB[1].')</option>';
   	for($i = $partsB[0]; $i <= $partsB[1]; $i++){
   		$r .= '<option value="'.$i.'" class="week-select-option" >'.$i.'</option>';
@@ -638,7 +671,10 @@ class iform_ukbms_section_plot {
 
   private static function _load_data_button($args, $auth, $nid, $options)
   {
-  	return '<input type="button" id="loadButton" class="loadButton" value="'.lang::get('Load Data').'"/>';
+    return '<input type="button" id="loadButton" class="loadButton" '.
+           'value="'.lang::get('Fetch Data').'" '.
+           'title="'.lang::get('Click this button to fetch the data from the database in order to display the graph. You must specify a year and site combination before you can fetch the data.').'" '.
+           '/>';
   }
   
   private static function _build_primary_toolbar($args, $auth, $nid, &$options)
@@ -688,6 +724,7 @@ class iform_ukbms_section_plot {
     data_entry_helper::add_resource('jqplot');
     data_entry_helper::add_resource('jqplot_bar');
     data_entry_helper::add_resource('jqplot_category_axis_renderer');
+    data_entry_helper::add_resource('jquery_cookie');
     $renderer='$.jqplot.BarRenderer';
     
     $auth = report_helper::get_read_auth($args['website_id'], $args['password']);
@@ -716,7 +753,9 @@ class iform_ukbms_section_plot {
       'noDataMsg' => lang::get('No data available'),
       'allWeeksDescription' => lang::get('All Weeks'),
       'seasonWeeksDescription' => lang::get('In-season Weeks'),
-      'dataLoadedMsg' => lang::get('Data Currently Loaded'),
+      'noDataLoadedMsg' => lang::get('There is no data available for'),
+      'selectPrompt' => lang::get('Select at least one species in the toolbar above in order to display the graph here.'),
+      'bodyWarning' => lang::get('Please fetch the data for a different year/site combination in order to display a graph.'),
       'class' => 'ui-widget ui-widget-content report-grid',
       'extraParams' => array(),
       'reportExtraParams' => '',
@@ -736,7 +775,9 @@ class iform_ukbms_section_plot {
       'species1SelectID' => 'usp-species1-select-'.$nid,
       'species2SelectID' => 'usp-species2-select-'.$nid,
       'weekNumSelectID' => 'usp-week-number-select-'.$nid,
-      'allSpeciesMsg' => lang::get("All Species")
+      'allSpeciesMsg' => lang::get("All Species"),
+      'reportGroup' => isset($args['report_group']) ? $args['report_group'] : '',
+      'rememberParamsReportGroup' => isset($args['remember_params_report_group']) ? $args['remember_params_report_group'] : ''
     );
 
     self::_set_up_survey_mapping($args, $auth, $options);
@@ -779,7 +820,7 @@ class iform_ukbms_section_plot {
     else
     	$opts['legend'] = array("show"=>true, 'placement'=>'outsideGrid');
     $opts['series'] = array();
-    $opts['title'] = array("text"=>"Title");
+//    $opts['title'] = array("text"=>"Title");
     $optsToCopyThrough = array('legend'=>'legendOptions', 'series'=>'seriesOptions', 'seriesColors'=>'seriesColors');
     foreach ($optsToCopyThrough as $key=>$settings) {
     	if (!empty($options[$settings]))
@@ -811,14 +852,16 @@ uspPrepChart(" . str_replace(array('"$.jqplot.CategoryAxisRenderer"','"$.jqplot.
 	    				self::_build_primary_toolbar($args, $auth, $nid, $options) .
     				'</thead>' .
     			'</table>'."\n".
-    			'<h2 id="currentlyLoaded">'.lang::get('No data currently loaded.').'</h2>' .
     			'<table id="secondary-controls-table" class="ui-widget ui-widget-content ui-corner-all controls-table">' .
     				'<thead class="ui-widget-header">' .
-    					self::_build_secondary_toolbar($args, $auth, $nid, $options) .
+    			    '<tr><th colspan=3><h2 id="currentlyLoaded">'.lang::get('Please fetch the data for a year/site combination.').'</h2></th></tr>' .
+    				  self::_build_secondary_toolbar($args, $auth, $nid, $options) .
     				'</thead>' .
     			'</table>'.
-    			'<div class="'.$options['class'].'" style="'.$widthStyle.'">' .
-    				'<div id="'.$options['id'].'" style="'.$heightStyle.' '.$widthStyle.'" class="jqplot-target"></div>' .
+    			'<div id="usp-chart" class="'.$options['class'].'" style="'.$widthStyle.'">' .
+    				'<div id="'.$options['id'].'" style="'.$heightStyle.' '.$widthStyle.'" class="jqplot-target">'.
+    				  '<p class="graph-body-warning">'.lang::get('You must fetch the data for a year/site combination before you can display a graph.').'</p>'.
+    				'</div>' .
     			"</div>\n";
 
 	return $retVal;
