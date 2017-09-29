2bf67fa4883404dbe4793814693c2e992f4184e4
John van Breda <john.vanbreda@biodiverseit.co.uk>
2017-09-29 15:37:08 +0100

    i18n for group edit
    Closes https://github.com/indicia-team/client_helpers/pull/57

---
+++ prebuilt_forms/lang/group_admin.de.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Language terms for group edit page in German.
+ *
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package Client
+ * @author Indicia Team
+ * @license http://www.gnu.org/licenses/gpl.html GPL 3.0
+ * @link http://code.google.com/p/indicia/
+ */
+
+global $custom_terms;
+
+/**
+ * Language terms for the group_edit form.
+ *
+ * @package Client
+ */
+$custom_terms = array(
+  "group's" => "Aktivitäten",
+  "groups" => "Aktivitäten",
+  'Set user to be a ' => 'Ändern zu ',
+  'Set user to be an ' => 'Ändern zu ',
+  'Remove from group' => 'Aus der Gruppe entfernen',
+  'Role' => 'Rolle',
+  'Access level' => 'Zugriffsberechtigung',
+  'Administer' => 'Verwalten',
+  'Administer {1}' => '{1} verwalten',
+  'You are trying to edit a group you don\'t have admin rights to.' =>
+    'Sie versuchen eine Gruppe zu bearbeiten, haben aber nicht die erforderlichen Administrator-Rechte dazu.',
+);
