5c9b964f5c6de97be7a99d2a60d71e0def86b90e
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-06-28 12:53:09 +0100

    IndiciaData includes user_id for warehouse access

--- helper_base.php
+++ helper_base.php
@@ -1621,8 +1621,11 @@ indiciaData.warehouseUrl='" . self::$base_url . "';
 indiciaData.protocol='$protocol';
 indiciaData.jQuery = jQuery; //saving the current version of jQuery
 ";
-      if(!empty(self::$website_id)) // not on warehouse
-          $script .= "indiciaData.website_id = " . self::$website_id . ";\n";
+      if (!empty(self::$website_id)) {
+        // not on warehouse
+        $script .= "indiciaData.website_id = " . self::$website_id . ";\n";
+        $script .= "indiciaData.user_id = " . hostsite_get_user_field('indicia_user_id') . ";\n";
+      }
 
       if (self::$js_read_tokens) {
         if (!empty(parent::$warehouse_proxy))
