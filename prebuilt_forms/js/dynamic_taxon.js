22350e83f8f72bce2b596999e59c10aca2f8a128
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-07-19 10:07:37 +0100

    JS to load taxon dynamic attrs

---
+++ prebuilt_forms/js/dynamic_taxon.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function docReady($) {
+  function changeTaxonRestrictionInputs() {
+    var urlSep = indiciaData.ajaxUrl.indexOf('?') === -1 ? '?' : '&';
+    if ($('#taxa_taxon_list\\:parent_id').val() !== '') {
+      $.each($('.taxon-dynamic-attributes'), function loadAttrDiv() {
+        var div = this;
+        // 0 is a fake nid, since we don't care.
+        $.get(indiciaData.ajaxUrl + '/dynamicattrs/0' + urlSep +
+            'taxon_list_id=' + $('#taxa_taxon_list\\:taxon_list_id').val() +
+            '&taxa_taxon_list_id=' + $('#taxa_taxon_list\\:parent_id').val() +
+            '&options=' + JSON.stringify(indiciaData.dynamicAttrOptions), null,
+          function getAttrsReportCallback(data) {
+            $(div).html(data);
+          }
+        );
+      });
+    }
+  }
+
+  // On selection of a taxon or change of sex/stage attribute, load any dynamically linked attrs into the form.
+  $('#taxa_taxon_list\\:parent_id').change(changeTaxonRestrictionInputs);
+});
