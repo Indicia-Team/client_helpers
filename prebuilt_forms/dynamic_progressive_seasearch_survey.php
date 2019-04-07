8a44c4d9aa68df4de8970776d07737a5cb269a92
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-04-07 14:08:42 +0100

    Fix syntax

--- prebuilt_forms/dynamic_progressive_seasearch_survey.php
+++ prebuilt_forms/dynamic_progressive_seasearch_survey.php
@@ -1493,7 +1493,7 @@ class iform_dynamic_progressive_seasearch_survey extends iform_dynamic_sample_oc
       // store some globals that we need later when creating uploaders
       $relpath = data_entry_helper::getRootFolder() . data_entry_helper::client_helper_path();
       $interimImageFolder = data_entry_helper::getInterimImageFolder('domain');
-      $relativeImageFolder = data_entry_helper::getImageRelativePath(),
+      $relativeImageFolder = data_entry_helper::getImageRelativePath();
       data_entry_helper::$javascript .= "indiciaData.uploadSettings = {\n";
       data_entry_helper::$javascript .= "  uploadScript: '" . $relpath . "upload.php',\n";
       data_entry_helper::$javascript .= "  destinationFolder: '$interimImageFolder',\n";
