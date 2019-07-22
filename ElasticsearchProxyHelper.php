c9019a3342d8e7b76407ea7051842d5ab3da0759
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-07-22 12:03:56 +0100

    Applies training mode filter to ES proxy
    See https://github.com/BiologicalRecordsCentre/ABLE/issues/95

--- ElasticsearchProxyHelper.php
+++ ElasticsearchProxyHelper.php
@@ -324,6 +324,10 @@ JS;
 
     }
     unset($query['bool_queries']);
+    // Apply a training mode filter.
+    $bool['must'][] = [
+      'term' => ['metadata.trial' => hostsite_get_user_field('training', FALSE)],
+    ];
     iform_load_helpers([]);
     $readAuth = helper_base::get_read_auth(self::$config['indicia']['website_id'], self::$config['indicia']['password']);
     self::applyPermissionsFilter($bool);
