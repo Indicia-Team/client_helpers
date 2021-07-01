b7c665eb02363d73800d9edadc107f342c7ff7b9
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-07-01 15:48:21 +0100

    New species alert option to filter by survey

--- prebuilt_forms/subscribe_species_alert.php
+++ prebuilt_forms/subscribe_species_alert.php
@@ -53,7 +53,7 @@ class iform_subscribe_species_alert {
       [
         [
           'fieldname' => 'list_id',
-          'label' => 'Species list ',
+          'label' => 'Species list',
           'helpText' => 'The species list that individual species can be picked from to receive alerts for.',
           'type' => 'checkbox_group',
           'table' => 'taxon_list',
@@ -110,6 +110,14 @@ class iform_subscribe_species_alert {
           'type' => 'boolean',
           'required' => FALSE,
           'default' => FALSE,
+          'group' => 'Lookups',
+        ],
+        [
+          'fieldname' => 'allow_survey_limit',
+          'label' => 'Allow survey limit',
+          'helpText' => 'Allow the user to create species alerts that only occur for data in a chosen survey.',
+          'type' => 'checkbox',
+          'required' => FALSE,
           'group' => 'Lookups',
         ],
       ]
@@ -205,6 +213,24 @@ class iform_subscribe_species_alert {
       ]);
     }
     $form .= "<fieldset><legend>" . lang::get('Alert criteria') . ":</legend>\n";
+    // Optional survey limit control.
+    if (!empty($args['allow_survey_limit'])) {
+      $default = empty(data_entry_helper::$entity_to_load['species_alert:survey_id']) ? '' : data_entry_helper::$entity_to_load['species_alert:survey_id'];
+      $form .= data_entry_helper::select([
+        'label' => lang::get('Limit to survey'),
+        'helpText' => lang::get(
+            'If you only want to receive alerts for data in a particular survey, select it here.'
+        ),
+        'fieldname' => 'species_alert:survey_id',
+        'table' => 'survey',
+        'valueField' => 'id',
+        'captionField' => 'title',
+        'extraParams' => $auth['read'] + ['orderby' => 'title'],
+        'blankText' => lang::get('- All surveys -'),
+        'default' => $default,
+      ]);
+
+    }
     // Output the species selection control
     // Default after saving with a validation failure can be pulled direct from the post, but
     // when reloading we don't need a default taxa taxon list ID since we already know the meaning
@@ -383,6 +409,9 @@ class iform_subscribe_species_alert {
     ];
     if (!empty($_POST['species_alert:id'])) {
       $params['id'] = $_POST['species_alert:id'];
+    }
+    if (!empty($_POST['species_alert:survey_id'])) {
+      $params['survey_id'] = $_POST['species_alert:survey_id'];
     }
     if (!empty($_POST['species_alert:taxon_list_id'])) {
       $params['taxon_list_id'] = $_POST['species_alert:taxon_list_id'];
