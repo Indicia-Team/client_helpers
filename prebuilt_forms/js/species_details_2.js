4715c76e964bb488c250abad71628b8b9a33a25d
burkmarr <rich.burkmar@gmail.com>
2022-12-09 04:28:42 -0800

    Added taxon selector to species_details_2

--- prebuilt_forms/js/species_details_2.js
+++ prebuilt_forms/js/species_details_2.js
@@ -1,16 +1,29 @@
 jQuery(document).ready(function($) {
 
+  // Adjust CSS on some form elements
+  $('#ctrl-wrap-occurrence-taxa_taxon_list_id').css('margin-bottom', 0)
+  $('#submit-taxon-search-form').css('margin-bottom', '15px')
+  $('#taxon-explore-records').css('margin-bottom', '15px')
+
+  // Reset GET form URL parameters before submission (species selector)
+  window.indiciaFns.speciesDetailsSub = () => {
+    $('#taxon-search-form #taxa_taxon_list_id').val($('#occurrence\\:taxa_taxon_list_id').val())
+    document.forms["taxon-search-form"].submit()
+  }
+
   var hectadData;
   var brcmap, brcyearly, brcphenology;
 
   // Translate any attribute names
-  fieldTransations = {};
-  $('#species-details-fields-translate').val().split(';').forEach(function(t) {
-    var ft = t.split('|');
-    if (ft.length === 2) {
-      fieldTransations[ft[0].trim()] = ft[1].trim();
-    }
-  })
+  ieldTransations = {};
+  if ($('#species-details-fields-translate').val()) {
+    $('#species-details-fields-translate').val().split(';').forEach(function(t) {
+      var ft = t.split('|');
+      if (ft.length === 2) {
+        fieldTransations[ft[0].trim()] = ft[1].trim();
+      }
+    })
+  }
   // Show non-duplicate attr names in species details table
   // and do any translation
   var prevAttr;
