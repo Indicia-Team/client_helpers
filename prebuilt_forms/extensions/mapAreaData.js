b1b6ab21f593024f7b1bdcf97fca5edaa17db2a1
John van Breda <john.vanbreda@biodiverseit.co.uk>
2024-05-22 09:05:01 +0100

    Switch projection 29901 -> 29903
    See https://github.com/Indicia-Team/warehouse/issues/510

--- prebuilt_forms/extensions/mapAreaData.js
+++ prebuilt_forms/extensions/mapAreaData.js
@@ -14,9 +14,9 @@
 indiciaData.areaPickerMapAreaData = {
   England: {bounds: [-729000, 6410000, 208000, 7516000], system: 'OSGB', projection: 27700},
   'Channel Islands': {bounds: [-314000, 6293000, -220400, 6418000], system: 'utm30ed50', projection: 23030},
-  Ireland: {bounds: [-1201000, 6671000, -608000, 7452000], system: 'OSIE', projection: 29901},
+  Ireland: {bounds: [-1201000, 6671000, -608000, 7452000], system: 'OSIE', projection: 29903},
   'Isle of Man': {bounds: [-554000, 7164000, -472600, 7266000], system: 'OSGB', projection: 27700},
-  'Northern Ireland': {bounds: [-924000, 7178000, -608000, 7452000], system: 'OSIE', projection: 29901},
+  'Northern Ireland': {bounds: [-924000, 7178000, -608000, 7452000], system: 'OSIE', projection: 29903},
   Scotland: {bounds: [-885000, 7240000, -17000, 8650000], system: 'OSGB', projection: 27700},
   Wales: {bounds: [-594000, 6681000, -266000, 7098000], system: 'OSGB', projection: 27700}
 };
