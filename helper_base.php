dd02bd7ea6456693af46d0b1c63b6fb23aaeccd0
John van Breda <john.vanbreda@biodiverseit.co.uk>
2024-09-30 15:58:56 +0100

    cachePerUser option fix

--- helper_base.php
+++ helper_base.php
@@ -3596,6 +3596,7 @@ if (typeof validator!=='undefined') {
       ];
       if (isset($options['cachePerUser']) && !$options['cachePerUser']) {
         $excludedParams[] = 'user_id';
+        $excludedParams[] = 'currentUser';
       }
       $cacheOpts = array_diff_key(array_merge($get, $post), array_combine($excludedParams, $excludedParams));
       $cacheOpts['serviceCallPath'] = self::$base_url . $url;
