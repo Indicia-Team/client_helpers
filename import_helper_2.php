6c63785ecec98cfd7a256538ae98a47a38209a65
Andrew van Breda <support@avb-it.co.uk>
2024-10-31 14:48:53 +0000

    Fix warnings appearing in Drupal log, not functionality differences

--- import_helper_2.php
+++ import_helper_2.php
@@ -539,7 +539,8 @@ HTML;
     // Get a list of imports that are reversible.
     $lookupData = report_helper::get_report_data([
       'dataSource' => 'library/imports/reversible_imports_list',
-      'extraParams' => $options['readAuth'] + $extraParams,
+      'readAuth' => $options['readAuth'],
+      'extraParams' => $extraParams,
     ]);
     self::addLanguageStringsToJs('import_helper_2', [
       'are_you_sure_reverse' => 'Are you sure you want to reverse the selected import?',
@@ -559,7 +560,7 @@ HTML;
     foreach ($lookupData as $importRow) {
       $reversableImports[$importRow['import_guid']] = 'Date: ' . $importRow['import_date_time'] . ' (Import ID: ' . $importRow['import_guid'] . ')';
     }
-    $r .= <<<HTML
+    $r = <<<HTML
       <hr>
       <p>
         $lang[select_a_previous_import_to_reverse]
@@ -642,11 +643,13 @@ HTML;
     // Has any of the data been changed since the import was done.
     $smpsChangedSinceImport = report_helper::get_report_data([
       'dataSource' => 'library/imports/changed_smps_since_import',
-      'extraParams' => $options['readAuth'] + $extraParams,
+      'readAuth' => $options['readAuth'],
+      'extraParams' => $extraParams,
     ]);
     $occsChangedSinceImport = report_helper::get_report_data([
       'dataSource' => 'library/imports/changed_occs_since_import',
-      'extraParams' => $options['readAuth'] + $extraParams,
+      'readAuth' => $options['readAuth'],
+      'extraParams' => $extraParams,
     ]);
     // If no changes have been made to the imported data,
     // then we can skip straight to the result page.
@@ -733,7 +736,7 @@ HTML;
     $response = self::http_post($serviceUrl, $data, FALSE);
     $output = json_decode($response['output'], TRUE);
     $reverseGuid = $_POST['reverse-guid'];
-    $r .= <<<HTML
+    $r = <<<HTML
       <form id="reversal-result-form" method="POST">
     HTML;
     if (!$response['result']) {
