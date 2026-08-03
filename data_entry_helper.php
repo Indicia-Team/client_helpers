f0bad41e88651c5b32ab7c709b2a60b93d082bab
Mark Alliston <marall@ceh.ac.uk>
2026-08-03 11:44:55 +0100

    A @minDate  parameter has been added to the date picker

--- data_entry_helper.php
+++ data_entry_helper.php
@@ -996,6 +996,7 @@ JS;
       'default' => '',
       'isFormControl' => TRUE,
       'allowFuture' => FALSE,
+      'minDate' => '',      
       'attributes' => [],
       'vagueLabel' => lang::get('Vague date mode'),
     ], $options);
@@ -1030,6 +1031,12 @@ JS;
       $dateTime = new DateTime();
       $attrArrayDate[] = 'max="' . $dateTime->format('Y-m-d') . '"';
     }
+
+    if (!empty($options['minDate'])) {
+      $attrArrayDate[] = 'min="' . $options['minDate'] . '"';
+    }
+
+
     $options['attribute_list'] = implode(' ', $attrArray);
     // Options for date control if using a free text vague date input.
     $options['attribute_list_date'] = implode(' ', $attrArrayDate);
